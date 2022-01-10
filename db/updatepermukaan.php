f8fee4c505281139a8470e8022d2f55b66845466
FirmanRizki <bloodseeker0095@gmail.com>
2022-01-10 20:47:52 +0700

    last code

--- db/updatepermukaan.php
+++ db/updatepermukaan.php
@@ -3,19 +3,19 @@
 include "connect_db.php";
 $var1 = $_GET['data1'];
 $statper = $_GET['data1'];
-if ($statper >0 && $statper<=20){
-    $ketper="BANJIR";
+if ($statper >0 && $statper<=10){
+    $ketper="AMAN";
+}
+else
+if ($statper >10 && $statper<=20){
+    $ketper="SIAGA 1";
 }
 else
 if ($statper >20 && $statper<=30){
     $ketper="SIAGA 2";
 }
-else
-if ($statper >30 && $statper<=40){
-    $ketper="SIAGA 1";
-}
 else {
-    $ketper="AMAN";
+    $ketper="BANJIR";
 }
 
 mysqli_query($conn, "INSERT INTO datapermukaan(permukaan,statusp) 
--- db/updatereservoir.php
+++ db/updatereservoir.php
@@ -5,16 +5,16 @@ $var2 = $_GET['data2'];
 $statres = $_GET['data2'];
 
 if ($statres >=0 && $statres<=10){
-    $ketres="RESERVOIR PENUH";
+    $ketres="RESERVOIR AMAN";
 }else
 if ($statres >10 && $statres<=15){
-    $ketres="RESERVOIR LEVEL 2";
+    $ketres="RESERVOIR LEVEL 1";
 }else
 if ($statres >15 && $statres<=20){
-    $ketres="RESERVOIR LEVEL 1";
+    $ketres="RESERVOIR LEVEL 2";
 }
 else {
-    $ketres="RESERVOIR AMAN";
+    $ketres="RESERVOIR PENUH";
 }
 
 mysqli_query($conn, "INSERT INTO datareservoir(reservoir,statusr) 
